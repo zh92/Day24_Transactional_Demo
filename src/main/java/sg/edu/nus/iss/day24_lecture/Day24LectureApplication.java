4b27d5313c6d24efa5f0add7ec33a36ee19530ee
zh92 <zhehao92@gmail.com>
2023-03-15 09:42:29 +0800

    Initial commit

---
+++ src/main/java/sg/edu/nus/iss/day24_lecture/Day24LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day24_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day24LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day24LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day24_lecture/Day24LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day24_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day24LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
